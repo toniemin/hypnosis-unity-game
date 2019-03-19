bb0963ee4a6a4ad34a7b12b58904d64ef2fc5ebb
Topi Nieminen <budy426@gmail.com>
2019-03-19 15:33:56 +0200

    Added player and floor
    Made a floor and a player sphere. The sphere has a light following it. Added movement with wasd.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody playerRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            playerRigidbody.AddForce(new Vector3(0, 0, 100f));
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            playerRigidbody.AddForce(new Vector3(100f, 0, 0));
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            playerRigidbody.AddForce(new Vector3(0, 0, -100f));
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            playerRigidbody.AddForce(new Vector3(-100f, 0, 100f));
+        }
+    }
+}
