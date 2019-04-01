9eafaccc1af7bfad08cc73690a71ef7bff1d5048
Topi Nieminen <budy426@gmail.com>
2019-04-01 22:12:12 +0300

    Expanded Sandbox to test camera controller.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player; // Reference to the player.
+
+    private GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector pointing towards player.
+        Vector3 playerDir = player.transform.position;
+
+        float angle = Vector3.SignedAngle(Vector3.forward, playerDir);
+         
+
+    }
+}
