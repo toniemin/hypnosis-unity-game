63e022007983fdb4c0d1377499fcd3bbcbe6829c
Topi Nieminen <budy426@gmail.com>
2019-04-03 01:01:42 +0300

    Continuing work on the camera controls.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     public GameObject player; // Reference to the player.
 
-    private float distance;
+    public float height = 20f; // Height in relation to the player (y-axis).
+
+    public float distance = 20f; // How far away from the player the camera is (z-axis).
+
+    private Vector3 position;
 
     void Awake()
     {
-        // Calculate distance between player and camera.
-        distance = Vector3.Distance(player.transform.position, transform.position);
+        // Get player's position.
+        Vector3 playerPos = player.transform.position;
+
+        // Set height in relation to player's position.
+        height -= playerPos.y;
+        // Set distance in relation to player's position.
+        distance += playerPos.z;
+        // Player's position on the x-asis.
+        float playerX = playerPos.x;
+
+        // Apply position.
+        position = new Vector3(playerX, height, distance);
+        transform.Translate(position);
+
+        // Turn camera towards the player.
+        transform.LookAt(playerPos);
+        
     }
 
     // Start is called before the first frame update
@@ -23,6 +41,12 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float playerX = player.transform.position.x;
+        if (position.x != playerX)
+        {
+            position.x = playerX;
+            transform.Translate(position);
+            transform.LookAt(player.transform.position);
+        }
     }
 }
