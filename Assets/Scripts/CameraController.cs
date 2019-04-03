c4dfd6df2ddf0b9bff29071e434c96828d77b0b0
Topi Nieminen <budy426@gmail.com>
2019-04-03 19:51:26 +0300

    First version of the camera controller finished.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,33 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player; // Reference to the player.
+    public Transform playerTransform; // Reference to player's transform for positioning.
 
     public float height = 20f; // Height in relation to the player (y-axis).
 
-    public float distance = 20f; // How far away from the player the camera is (z-axis).
-
-    private Vector3 position;
+    public float distance = -20f; // How far away from the player the camera is (z-axis).
 
     void Awake()
     {
         // Get player's position.
-        Vector3 playerPos = player.transform.position;
+        Vector3 playerPos = playerTransform.position;
 
-        // Set height in relation to player's position.
-        height -= playerPos.y;
-        // Set distance in relation to player's position.
-        distance += playerPos.z;
-        // Player's position on the x-asis.
-        float playerX = playerPos.x;
+        // Set camera position. Control distance using 'height' (for y-axis) and 'distance' (for z-axis) variables.
+        // The position on the x-asis is the same as the player.
+        transform.position = new Vector3(playerPos.x, playerPos.y + height, playerPos.z + distance);
 
-        // Apply position.
-        position = new Vector3(playerX, height, distance);
-        transform.Translate(position);
-
-        // Turn camera towards the player.
+        // Turn the camera towards the player.
         transform.LookAt(playerPos);
-        
     }
 
     // Start is called before the first frame update
@@ -41,12 +31,14 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float playerX = player.transform.position.x;
-        if (position.x != playerX)
-        {
-            position.x = playerX;
-            transform.Translate(position);
-            transform.LookAt(player.transform.position);
-        }
+        // Update player's position.
+        Vector3 playerPos = playerTransform.position;
+
+        // Update camera position. Control distance using 'height' (for y-axis) and 'distance' (for z-axis) variables.
+        // The position on the x-asis is the same as the player.
+        transform.position = new Vector3(playerPos.x, playerPos.y + height, playerPos.z + distance);
+
+        // Turn the camera towards the player.
+        transform.LookAt(playerPos);
     }
 }
