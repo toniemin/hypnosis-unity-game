e2cf2afeedbb59e4379e51ca15faf5ace1142b55
Topi Nieminen <nieminen.topi.m@student.uta.fi>
2019-04-30 15:58:23 +0300

    Implemented NPC movement via following a path. TODO: Make NPC rotate towards movement direction.

---
+++ Assets/Scripts/GuardController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardController : MonoBehaviour
+{
+    public Transform pathHolder;
+
+    private float speed = 10f;
+    private float waitTime = .3f;
+    private float turnSpeed = 90f;
+
+    private void Start()
+    {
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i].y = transform.position.y;
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+
+        Debug.Log("transform.position: " + transform.position.ToString() + ", waypoint pos: " + waypoints[2].ToString() + ", angle: " + Vector3.Angle(transform.position, waypoints[2]));
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 prevPosition = startPosition;
+
+        foreach (Transform waypoint in pathHolder)
+        {
+            Vector3 currentPosition = waypoint.position;
+
+            Gizmos.DrawSphere(currentPosition, .3f);
+            Gizmos.DrawLine(prevPosition, currentPosition);
+
+            prevPosition = currentPosition;
+        }
+
+        Gizmos.DrawLine(startPosition, prevPosition);
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+        transform.LookAt(waypoints[0]);
+
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if (transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = ++targetWaypointIndex % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirLookTarget = (lookTarget - transform.position).normalized;
+        float targetAngle = 90 - Mathf.Atan2(dirLookTarget.z, dirLookTarget.x) * Mathf.Rad2Deg;
+
+        while (Mathf.Approximately(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle), 0))
+        {
+            float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, turnSpeed * Time.deltaTime);
+            transform.eulerAngles = Vector3.up * angle;
+            yield return null;
+        }
+    }
+}
