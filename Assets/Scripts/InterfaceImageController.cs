c46c58786aa00543c3892816ef387c8355a398f3
Topi Nieminen <budy426@gmail.com>
2019-05-12 21:56:52 +0300

    Started working on tutorial and story image renderer.

---
+++ Assets/Scripts/InterfaceImageController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceImageController : MonoBehaviour
+{
+    public Sprite[] tutorials;
+    public Sprite[] stories;
+
+    public Image output;
+
+    private int tutorialIndex = 0;
+    private int storyIndex = 0;
+    
+
+    public void showTutorial()
+    {
+        if (! output.enabled)
+            output.enabled = true;
+        nextTutorial();
+    }
+
+    public void nextTutorial()
+    {
+        output.sprite = tutorials[tutorialIndex];
+        tutorialIndex++;
+
+        if (tutorialIndex >= tutorials.Length)
+        {
+            output.enabled = false;
+        }
+    }
+
+    public void showStory()
+    {
+        if (!output.enabled)
+            output.enabled = true;
+
+        nextStory();
+    }
+
+    public void nextStory()
+    {
+        output.sprite = stories[storyIndex];
+        storyIndex++;
+
+        if (storyIndex >= stories.Length)
+        {
+            output.enabled = false;
+        }
+    }
+}
