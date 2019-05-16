ee940cae49fa6d9303083441dd611cdd36dbb473
Topi Nieminen <budy426@gmail.com>
2019-05-16 18:41:03 +0300

    Added a restart function to EndScreen prefab. Edited all levels to have the new EndScreen and tested the level restart works.

---
+++ Assets/Scripts/LevelRestart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestart : MonoBehaviour
+{
+    public int currentSceneIndex;
+
+    public void reloadScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex, LoadSceneMode.Single);
+    }
+}
